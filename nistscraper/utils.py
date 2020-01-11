8311d900fb0f7af41d616d230d19e4fa057d8341
stickfighter342 <50229133+stickfighter342@users.noreply.github.com>
2020-01-11 10:05:45 +0800

    added conversion methods for temperature, pressure, and volume.

--- nistscraper/utils.py
+++ nistscraper/utils.py
@@ -14,3 +14,53 @@ def ideal_gas_law(T, Vm):
     return R * T / Vm
 
 
+def cTOall(value, key):
+    if (key == "temp"):  
+        return [value, value * 1.8 + 32, value + 273.15, (value + 273.15) * 1.8]
+    else if (key == "pres"):
+        return [value, value * 101.325, value * 1.01325, value * 14.6959]
+    
+
+def arrTemp(initial, temp):
+    if (initial == "r"):
+        temp = (temp - 491.67) / 1.8
+    elif (initial == "k"):
+        temp -= 273.15
+    elif (initial == "f"):
+        temp = (temp - 32) / 1.8
+    return cTOall(temp)    
+
+
+def convTemp(initial, temp, convert):
+    vec = arrTemp(initial, temp)
+    if (convert == "r"):
+        return vec[3]
+    elif (convert == "k"):
+        return vec[2]
+    elif (convert == "f"):
+        return vec[1]
+    return vec[0]
+
+def arrPres(initial, pres):
+    if (initial == "psig"):
+        pres /= 14.6959
+    elif (initial == "kPa"):
+        pres /= 101.325
+    elif (initial == "bar"):
+        pres /= 1.01325
+    return cTOall(pres, "pres")  
+    
+def convPres(initial, pres, convert):
+    vec = arrPres(initial, pres)
+    if (convert == "psig"):
+        return vec[3]
+    elif (convert == "bar"):
+        return vec[2]
+    elif (convert == "kPa"):
+        return vec[1]
+    return vec[0]
+
+def convVol(initial, vol, convert):
+    if (initial == "L/mol"):
+        return vol * 0.001
+    return vol * 1000
