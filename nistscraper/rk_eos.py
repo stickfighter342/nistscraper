6812cd2aaa570a1159dde6c809c1859fc8f493f5
Chad Curtis <ccurtis7@uw.edu>
2020-01-25 09:16:48 -0800

    Made a few changes to rk_eos

--- nistscraper/rk_eos.py
+++ nistscraper/rk_eos.py
@@ -3,21 +3,39 @@
 Created on Sat Jan 18 11:17:16 2020
 
 @author: 21EthanD
+
+Contains residual functions for several equations of state.
+Currently limited to RK EOS.
 """
 from scipy.optimize import fsolve
 import numpy as np
-r = 0.0821 #atm * L / mol / K
 
-def eos(p, t, v):
+Rgas = 0.0821 #atm * L / mol / K
+
+def eos(P, T, v):
     """
+    Returns the residual of the Redlich-Kwong equation of state.
+    
+    Parameters
+    ----------
+    P: Pressure in atm (int or list-like object)
+    
+    Returns
+    -------
+    res:
+    
+    Examples
+    --------
+    
     y = data[0]
     t = y[0]
     v = y[1]"""
-    tc = 126.2
-    pc = 33.5    
-    a = 0.42748 * r * r * pow(tc, 2.5) / pc
-    b = 0.08664 * r * tc / pc
-    return p + a / (pow(t, 0.5) * v * (v + b)) - r * t / (v - b)
+    Tc = 126.2
+    Pc = 33.5    
+    a = 0.42748*Rgas**2*Tc**2.5/Pc
+    b = 0.08664*Rgas*Tc/Pc
+    res = P + a/(T**0.5*v*(v + b)) - Rgas *T/(v - b)
+    return res
 
 
 a = fsolve(eos, 0, args=(200, 300));
